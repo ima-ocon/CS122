add63fd3b46c07eb2947b23a40a90b8a9a302253
ima-ocon <ima_ocon@yahoo.com>
2016-05-18 21:00:45 +0800

    Edited for Heroku deployment

--- tables.sql
+++ tables.sql
@@ -1,1 +1,11 @@
-DROP TABLE warehouse_staff;
+CREATE TABLE warehouse_staff
+(
+	staffID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	s_lastname VARCHAR(255) default 'galace',
+	s_MI VARCHAR(255) default 'n',
+	s_firstname VARCHAR(255) default 'guigi',
+	s_address VARCHAR(255) default 'quezon city',
+	s_contactno VARCHAR(11) default '09120987123'
+);
+
+ALTER TABLE warehouse_staff AUTO_INCREMENT=10001;
